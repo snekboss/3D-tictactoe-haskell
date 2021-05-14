80dbaf95299e94fec6912f255f7d0faf03919b9d
snekboss <62026503+snekboss@users.noreply.github.com>
2021-05-14 17:28:42 +0200

    Added some functions, and an incomplete game loop
    Added functions:
    - isOutOfBounds
    - setCell (not implemented)
    - getCell
    - isEmpty
    - gameStart
    - gameLoop
    
    -- TODO list ---
    - When reading input, the user literally has to type "(x, y, z)". This is cumbersome. It'd be better if it was just "x y z".
    
    - Implement setCell function.
    
    - Handle exceptions in game related functions (run, gameLoop).

--- tictactoe.hs
+++ tictactoe.hs
@@ -3,13 +3,31 @@ import Data.List
 {-
 
 [
-["---", "---", "---"],
-["---", "---", "---"],
-["---", "---", "---"]
+    [
+    "---",
+    "---",
+    "---"
+    ],
+    
+    [
+    "---",
+    "---",
+    "---"
+    ],
+    
+    [
+    "---",
+    "---",
+    "---"
+    ],
 ]
 
 -}
 
+
+emptyChar = '.'
+playerChar = 'X'
+computerChar = 'O'
 
 
 
@@ -46,23 +64,91 @@ toStrBoard (list : remListOfListOfStrings) = toStrOneList list ++ "\n" ++ toStrB
 
 
 
--- ============================== The game itself (hopefully) ==============================
+-- ============================== Board related functions (hopefully) ==============================
 --Arg1: Dimension N of the NxNxN tic-tac-toe board.
 --Ret: An empty board. For example, dims=3, then [["---", "---", "---"], ["---", "---", "---"], ["---", "---", "---"]].
 initBoard :: Int -> [[String]]
-initBoard dims = replicate dims (replicate dims (replicate dims '.'))
+initBoard dims = replicate dims (replicate dims (replicate dims (emptyChar)))
 
 
+--Arg1: The size of the board
+--Arg2: A tictactoe move triplet (x,y,z), where each coordinate is in the inclusive range [1, Arg2].
+--Ret: True if the move triplet is out of bounds.
+isOutOfBounds :: Int -> (Int, Int, Int) -> Bool
+isOutOfBounds size (x, y, z) = x < 1 || y < 1 || z < 1 || x > size || y > size || z > size
+
+
+--TODO: Yes, I need to implement this function...
+--Arg1: A tictactoe triplet (x,y,z), where each coordinate is in the inclusive range [1, (length Arg2)].
+--Arg2: A character value: '-' or 'X' or 'O'.
+--Arg3: A 3D tictactoe board.
+--Ret: The new tictactoe board, where the board at the triplet's coordinates now contains Arg2's value.
+--NOTES: The Arg1 triplet is assumed to be inside the boundaries of the board.
+setCell :: (Int, Int, Int) -> Char -> [[String]] -> [[String]]
+setCell (x, y, z) moveChar (face : remFaces) = [] -- TODO: My brain is fried...
+
+
+--Arg1: A tictactoe triplet (x,y,z), where each coordinate is in the inclusive range [1, (length Arg2)].
+--Arg2: A 3D tictactoe board.
+--Ret: A char equal to '.' or 'X' or 'O'.
+--NOTES: The Arg1 triplet is assumed to be inside the boundaries of the board.
+getCell :: (Int, Int, Int) -> [[String]] -> Char
+getCell (x, y, z) board = (( board !! (z - 1)) !! (y - 1)) !! (x - 1)
+
+
+--Arg1: A tictactoe move triplet (x,y,z), where each coordinate is in the inclusive range [1, (length Arg2)].
+--Arg2: A 3D tictactoe board.
+--Ret: True if the move is available on the board.
+--NOTES: The Arg1 triplet is assumed to be inside the boundaries of the board.
+isEmpty :: (Int, Int, Int) -> [[String]] -> Bool
+isEmpty triplet board = (getCell triplet board) == (emptyChar)
+
+
+
+-- ============================== Game loop, etc. ==============================
 --Desc: This is how you start the game.
 run = do
         putStr "Enter board size: "
         dims <- readLn :: IO Int -- TODO: Exception if bad input. Yay or nay?
-        if dims < 3
-        then do
+        if dims < 3 then do
             putStrLn "The size cannot be less than 3."
             return ()
         else do
             gameStart (initBoard 3)
 
 
-gameStart board = return ()
+--Desc: The start of the game. Do your initialization stuff here.
+gameStart board = do
+        gameLoop 1 board
+
+
+--Desc: Game loop. Godspeed.
+--Arg1: 1 for player1; 2 for player2.
+--Arg2: The current state of the game board.
+--Ret: Some monadic IO magic.
+gameLoop player board = do
+         putStr (toStrBoard board)
+         putStr "Your move (x y z)?: "
+         (x, y, z) <- readLn :: IO (Int, Int, Int) -- TODO: I must type (x, y, z). Also, exception if bad input. What do?
+         if isOutOfBounds (length board) (x, y, z) then do
+             putStrLn ""
+             putStrLn "Your move is out of bounds."
+             putStrLn ("Please fit your coordinates into the inclusive [1," ++ (show (length board)) ++ "] range.")
+             putStrLn ""
+             gameLoop player board
+         else if (isEmpty (x, y, z) board) == False then do
+             putStrLn ""
+             putStrLn "You cannot make your move there."
+             putStrLn ""
+             gameLoop player board
+         else do
+             {-
+             -- TODO: Write the main game loop plz. Win conditions, minimax, etc.
+             let boardPlayerMoved = setCell (x, y, z) (playerChar) board
+                 
+                 boardComputerMoved = 
+             -}
+             putStrLn "TODO: Implement the main game loop plz."
+             gameLoop (3 - player) board
+         
+         
