b57afb829a073f6cf948f3c3ccd93dbf9237de57
snekboss <62026503+snekboss@users.noreply.github.com>
2021-05-14 16:10:22 +0200

    Started the project, and added printing functions
    title

---
+++ tictactoe.hs
@@ -0,0 +1,68 @@
+import Data.List
+
+{-
+
+[
+["---", "---", "---"],
+["---", "---", "---"],
+["---", "---", "---"]
+]
+
+-}
+
+
+
+
+-- ============================== Board to String related stuff ==============================
+--Desc: This is a helper function, meant to be called by toStrOneRow.
+--Arg1: A row string.
+--Ret: An output string.
+toStrOneRow_helper :: String -> String
+toStrOneRow_helper [c] = [' ', c]
+toStrOneRow_helper (c : remStr) = [' ', c] ++ (toStrOneRow_helper remStr)
+
+
+--Desc: Takes a row like "-XO" and prints it like "- X O"
+--      Uses toStrOneRow_helper function to achieve this.
+--Arg1: A row like "-XO".
+--Ret: An output string like "- X O ".
+toStrOneRow :: String -> String
+toStrOneRow (c : remStr) = c : toStrOneRow_helper remStr
+
+
+--Arg1: One list of strings.
+--Ret: String output
+toStrOneList :: [String] -> String
+toStrOneList [s] = toStrOneRow s
+toStrOneList (s : remList) = (toStrOneRow s) ++ "   " ++ toStrOneList remList
+
+
+--Arg1: A game board.
+--Ret: Formatted output string of the board.
+toStrBoard :: [[String]] -> String
+toStrBoard [] = ""
+toStrBoard (list : remListOfListOfStrings) = toStrOneList list ++ "\n" ++ toStrBoard remListOfListOfStrings
+
+
+
+
+-- ============================== The game itself (hopefully) ==============================
+--Arg1: Dimension N of the NxNxN tic-tac-toe board.
+--Ret: An empty board. For example, dims=3, then [["---", "---", "---"], ["---", "---", "---"], ["---", "---", "---"]].
+initBoard :: Int -> [[String]]
+initBoard dims = replicate dims (replicate dims (replicate dims '.'))
+
+
+--Desc: This is how you start the game.
+run = do
+        putStr "Enter board size: "
+        dims <- readLn :: IO Int -- TODO: Exception if bad input. Yay or nay?
+        if dims < 3
+        then do
+            putStrLn "The size cannot be less than 3."
+            return ()
+        else do
+            gameStart (initBoard 3)
+
+
+gameStart board = return ()
